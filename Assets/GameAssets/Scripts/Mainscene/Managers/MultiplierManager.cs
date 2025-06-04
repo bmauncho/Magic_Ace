5be737c3c7c372043ba4613d397f223defabce9f
BradleyMauncho <84400423+bmauncho@users.noreply.github.com>
2025-06-04 14:52:17 +0300

    Update Multiplier

--- Assets/GameAssets/Scripts/Mainscene/Managers/MultiplierManager.cs
+++ Assets/GameAssets/Scripts/Mainscene/Managers/MultiplierManager.cs
@@ -1,31 +1,34 @@
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public enum Multipliers
 {
-    X1 = 1,
-    X2 = 2,
-    X3 = 3,
-    X4 = 4,
-    X5 = 5,
-    X6 = 6,
-    X7 = 7,
-    X8 = 8,
-    X9 = 9,
-    X10 = 10,
-    X11 = 11,
-    X12 = 12,
-    X13 = 13,
-    X14 = 14,
-    X15 = 15,
-    X16 = 16,
-    X17 = 17,
-    X18 = 18,
-    X19 = 19,
-    X20 = 20
+    x1 = 1,
+    x2 = 2,
+    x3 = 3,
+    x4 = 4,
+    x5 = 5,
+    x6 = 6,
+    x7 = 7,
+    x8 = 8,
+    x9 = 9,
+    x10 = 10,
+    x11 = 11,
+    x12 = 12,
+    x13 = 13,
+    x14 = 14,
+    x15 = 15,
+    x16 = 16,
+    x17 = 17,
+    x18 = 18,
+    x19 = 19,
+    x20 = 20
 }
 
 public enum MultiplierType
@@ -40,92 +43,180 @@ public class MultiplierCanonInfo
 {
     public MultiplierType multiplierType;
     [Tooltip("used for canon 1 & 4")]
-    public Sprite canon_1;
+    public Sprite canon_1_active;
+    public Sprite canon_1_inactive;
     [Tooltip("used for canon 2 & 3")]
-    public Sprite canon_2;
+    public Sprite canon_2_active;
+    public Sprite canon_2_inactive;
 }
 
 public class MultiplierManager : MonoBehaviour
 {
-    [SerializeField] private Multipliers currentMultiplier = Multipliers.X1;
+    [SerializeField] private Multipliers activeMultiplier = Multipliers.x1;
     [SerializeField] private MultiplierType currentType = MultiplierType.Normal;
-    [SerializeField] private MultiplierCanonInfo[] multiplierCanonInfos;
     [SerializeField] private TopBanner topBanner_;
+    [SerializeField] private TMP_SpriteAsset spriteAsset_active;
+    [SerializeField] private TMP_SpriteAsset spriteAsset_inactive;
+    [SerializeField] private List<Multipliers> orderOfMultipliers = new List<Multipliers>();
+    [SerializeField] private MultiplierCanonInfo[] multiplierCanonInfos;
     private static readonly Dictionary<MultiplierType , List<Multipliers>> multiplierProgressions =
         new Dictionary<MultiplierType , List<Multipliers>>()
         {
-            { MultiplierType.Normal,   new List<Multipliers> { Multipliers.X1, Multipliers.X2, Multipliers.X3, Multipliers.X5 } },
-            { MultiplierType.Free, new List<Multipliers> { Multipliers.X2, Multipliers.X4, Multipliers.X6, Multipliers.X10 } }
+            { MultiplierType.Normal,   new List<Multipliers> { Multipliers.x1, Multipliers.x2, Multipliers.x3, Multipliers.x5 } },
+            { MultiplierType.Free, new List<Multipliers> { Multipliers.x2, Multipliers.x4, Multipliers.x6, Multipliers.x10 } }
         };
-
-    public Multipliers GetCurrentMultiplier () => currentMultiplier;
+    public Multipliers GetActiveMultiplier () => activeMultiplier;
     public MultiplierType GetCurrentType () => currentType;
-
     private int cannonIndex = 0;
+    [SerializeField] private bool hasRecalled = false;
     private int lastMultiplierIndex = -1;
+    [ContextMenu("Reset Multiplier")]
     public void ResetMultiplier ()
     {
-        currentMultiplier = multiplierProgressions [currentType] [0];
-    }
-
-    public void AdvanceMultiplier ()
-    {
-        var list = multiplierProgressions [currentType];
-        int index = list.IndexOf(currentMultiplier);
-        if (index >= 0 && index < list.Count - 1)
+        orderOfMultipliers.Clear();
+        activeMultiplier = multiplierProgressions [currentType] [0];
+        cannonIndex = 0;
+        hasRecalled = false;
+        for (int i =0 ;i<4 ;i++)
         {
-            currentMultiplier = list [index + 1];
+            updateUI(true);
         }
     }
-
-    public void ApplyCollectorEffect ()
-    {
-        int value = Mathf.Min((int)currentMultiplier + 1 , 20);
-        currentMultiplier = (Multipliers)value;
-    }
-
     public void SetMultiplierType ( MultiplierType type )
     {
         currentType = type;
         ResetMultiplier();
     }
 
-    public Multipliers GetNextMultiplier ()
+    public void AdvanceMultiplier ()
     {
         var list = multiplierProgressions [currentType];
-        int index = list.IndexOf(currentMultiplier);
+        int index = list.IndexOf(activeMultiplier);
         if (index >= 0 && index < list.Count - 1)
         {
-            return list [index + 1];
+            if (!hasRecalled) 
+            {
+                hasRecalled = true;
+                return; 
+            } 
+            activeMultiplier = list [index + 1];
         }
-        return currentMultiplier;
+    }
+    public void ApplyCollectorEffect ()
+    {
+        int value = Mathf.Min((int)activeMultiplier + 1 , 20);
+        activeMultiplier = (Multipliers)value;
     }
 
-    public void updateUI ()
+    [ContextMenu("Test")]
+    public void Test ()
     {
-
+        AdvanceMultiplier();
+        
+        for (int i = 0 ; i < 4 ; i++)
+        {
+            updateUI();
+        }
+    }
+    /// <summary>
+    /// This method is called 4 times to update the UI for each cannon.
+    /// </summary>
+    public void updateUI (bool isReset = false)
+    {
+        cannonType cannon = (cannonType)cannonIndex;
+        var list = multiplierProgressions [currentType];
+        Multipliers multiplier = list [cannonIndex];
+        topBanner_.UpdateCanon(cannon , multiplier,activeMultiplier ,GetCannon() ,GetCanonBg() , GetSpriteAsset(),isReset);
+        cannonIndex++;
+        Debug.Log(cannonIndex);
         if (cannonIndex > 3)
         {
             cannonIndex = 0; // Reset cannon index if it exceeds the number of cannons
         }
-
-        cannonType cannon = (cannonType)cannonIndex;
-
-        // Determine which sprite to use based on cannon type
-        Sprite cannonSprite = ( cannon == cannonType.cannon1 || cannon == cannonType.cannon4 )
-            ? multiplierCanonInfos [0].canon_1
-            : multiplierCanonInfos [0].canon_2;
-
-        //canon1 = current multiplier
-        //canon2 = next multipler according to logic
-        //canon3 = next multiplier after cannon2 multiplier according to logic
-        //canon4 = next multiplier after cannon3 multiplier according to logic
-        // also aplies to cannon multiplier
-        string multiplier = currentMultiplier.ToString();
-
-        Multipliers cannonMultiplier = currentMultiplier++;
-
-        topBanner_.UpdateCanon(cannon , currentMultiplier , multiplier , cannonSprite);
-        cannonIndex++;
     }
+
+    private Sprite GetCannon ()
+    {
+        Sprite cannon = null;
+        var list = multiplierProgressions [currentType];
+        int activeIndex = list.IndexOf(activeMultiplier);
+        bool isActive = cannonIndex == activeIndex;
+
+        //Debug.Log($"Cannon Index: {cannonIndex}, Active Index: {activeIndex}, Is Active: {isActive}");
+
+        int typeIndex = currentType switch
+        {
+            MultiplierType.Normal => 0,
+            MultiplierType.Free => 1,
+            MultiplierType.collector => 2,
+            _ => -1
+        };
+
+        if (typeIndex < 0 || typeIndex >= multiplierCanonInfos.Length)
+            return null;
+
+        var info = multiplierCanonInfos [typeIndex];
+
+        if (cannonIndex == 0 || cannonIndex == 3) // Cannon 1 & 4
+        {
+            cannon = isActive ? info.canon_1_active : info.canon_1_inactive;
+        }
+        else if (cannonIndex == 1 || cannonIndex == 2) // Cannon 2 & 3
+        {
+            cannon = isActive ? info.canon_2_active : info.canon_2_inactive;
+        }
+
+        return cannon;
+    }
+
+    public Sprite GetCanonBg ()
+    {
+        Sprite cannon = null;
+        int typeIndex = currentType switch
+        {
+            MultiplierType.Normal => 0,
+            MultiplierType.Free => 1,
+            MultiplierType.collector => 2,
+            _ => -1
+        };
+        var info = multiplierCanonInfos [typeIndex];
+        if (cannonIndex == 0 || cannonIndex == 3) // Cannon 1 & 4
+        {
+            cannon =  info.canon_1_inactive;
+        }
+        else if (cannonIndex == 1 || cannonIndex == 2) // Cannon 2 & 3
+        {
+            cannon =  info.canon_2_inactive;
+        }
+        return cannon;
+    }
+
+
+    private TMP_SpriteAsset GetSpriteAsset ()
+    {
+        TMP_SpriteAsset asset = null;
+        var list = multiplierProgressions [currentType];
+        int activeIndex = list.IndexOf(activeMultiplier);
+        bool isActive = cannonIndex == activeIndex;
+        if (isActive)
+        {
+            asset = spriteAsset_active;
+        }
+        else
+        {
+           asset = spriteAsset_inactive;
+        }
+        return asset;
+    }
+    public Multipliers GetNextMultiplier ()
+    {
+        var list = multiplierProgressions [currentType];
+        int index = list.IndexOf(activeMultiplier);
+        if (index >= 0 && index < list.Count - 1)
+        {
+            return list [index + 1];
+        }
+        return activeMultiplier;
+    }
+
 }
--- Assets/GameAssets/Scripts/Mainscene/TopBanner/TopBanner.cs
+++ Assets/GameAssets/Scripts/Mainscene/TopBanner/TopBanner.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,25 +21,128 @@ public class CannonData
     public GameObject cannon;
     public TMP_Text cannonMultiplier;
     public Image cannonImage;
+    public Image cannonImageBg;
+    public Animator Anim;
 }
 public class TopBanner : MonoBehaviour
 {
     [Header("Cannon Data")]
     public CannonData [] cannons;
-    
-    public void UpdateCanon(cannonType cannonType,Multipliers multipliers,string theMultiplier,Sprite thecanon)
+    int activeIndex = -1;
+    public void UpdateCanon(cannonType cannonType,Multipliers multipliers,
+        Multipliers activemultiplier,Sprite thecanon,Sprite thecanonBg, TMP_SpriteAsset spriteAsset,bool isReset = false)
     {
-        for(int i=0 ; i <cannons.Length ; i++)
+        StartCoroutine(updateUi(cannonType , multipliers , activemultiplier , thecanon,thecanonBg , spriteAsset,isReset));
+    }
+
+    private IEnumerator updateUi ( cannonType cannonType , Multipliers multipliers ,
+        Multipliers activeMultiplier , Sprite cannonSprite , Sprite cannonBgSprite , TMP_SpriteAsset spriteAsset ,bool isReset = false )
+    {
+        // First, find the active cannon index for reference
+        for (int i = 0 ; i < cannons.Length ; i++)
         {
-            if (cannons [i].type == cannonType)
+            if (cannons [i].type == cannonType && cannons [i].currentMultiplier == activeMultiplier)
             {
-                cannons [i].currentMultiplier = multipliers;
-                cannons [i].cannonMultiplier.text = theMultiplier;
-                if (cannons [i].cannonImage != null)
-                {
-                    cannons [i].cannonImage.sprite = thecanon;
-                }
+                activeIndex = i;
+                break;
             }
         }
+        Debug.Log("Active index : " + activeIndex);
+        List<int> previousIndices = new List<int>();
+
+        for (int i = 0 ; i < activeIndex && i < cannons.Length ; i++)
+        {
+            previousIndices.Add(i);
+        }
+        Debug.Log("previous cannons count: " + previousIndices.Count);
+
+
+        for (int i = 0 ; i < cannons.Length ; i++)
+        {
+            if (cannons [i].type != cannonType)
+                continue;
+
+            var cannon = cannons [i];
+            cannon.currentMultiplier = multipliers;
+            cannon.cannonMultiplier.spriteAsset = spriteAsset;
+
+            bool isActive = ( activeMultiplier == multipliers );
+            cannon.cannonMultiplier.text = SetText(multipliers.ToString() , isActive);
+
+            if (isActive && !isReset)
+            {
+                string animName = $"Canon_{i + 1}_Anim";
+                Debug.Log($"Playing animation: {animName} for ACTIVE cannon type: {cannonType}");
+                StartCoroutine(PlayAnim(cannon.Anim , animName));
+                if (previousIndices == null || previousIndices.Count == 0)
+                {
+                    Debug.Log("prev cannon is null");
+                }
+                else
+                {
+                    Debug.Log("previous cannons count: " + previousIndices.Count);
+                    // Play the previous cannon's animation if it exists and is not the same as the current cannon
+                    foreach (int index in previousIndices)
+                    {
+                        var prevCannon = cannons [index];
+                        if (prevCannon == cannon) continue;
+
+                        string prevAnimName = $"Canon_{index + 1}_inactive";
+                        Debug.Log($"Playing animation: {prevAnimName} for INACTIVE cannon type: {cannonType}");
+                        StartCoroutine(PlayAnim(prevCannon.Anim , prevAnimName));
+                    }
+                }
+            }
+ 
+            if (cannon.cannonImage != null)
+                cannon.cannonImage.sprite = cannonSprite;
+
+            if (cannon.cannonImageBg != null)
+                cannon.cannonImageBg.sprite = cannonBgSprite;
+        }
+        yield return null;
+    }
+    private IEnumerator PlayAnim ( Animator anim , string whichAnim )
+    {
+        anim.Rebind();
+        anim.Play(whichAnim);
+
+        // Wait until the animator updates (next frame)
+        yield return null;
+
+        // Wait for the animation to finish
+        while (true)
+        {
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+            if (stateInfo.IsName(whichAnim) && stateInfo.normalizedTime >= 1f)
+            {
+                Debug.Log("Animation is complete!");
+                break;
+            }
+
+            yield return null; // Wait for the next frame
+        }
+    }
+
+    private string SetText ( string text , bool isActive )
+    {
+        string result = string.Empty;
+        if (isActive)
+        {
+            foreach (char c in text)
+            {
+                result += $"<sprite name=Base_Muiltiple_FNT_{c}>";
+            }
+        }
+        else
+        {
+            foreach (char c in text)
+            {
+                result += $"<sprite name=Base_Muiltiple_D_FNT_{c}>";
+            }
+        }
+
+        return result;
     }
 }
